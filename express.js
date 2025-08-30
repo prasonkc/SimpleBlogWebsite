8a1fe8f2c35e99c1e22c7825e395a9bc9ceaad8b
Prason <prasonkc07@gmail.com>
2025-08-30 10:00:41 +0545

    handled crud operations

--- express.js
+++ express.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mongoose = require("mongoose")
+const Blog = require("./models/blog");
+const { result } = require('lodash');
 
 // Express App
 const app = express()
@@ -7,6 +10,85 @@ const app = express()
 // Register view Engines
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views'); //if views are in a seperate folder
+
+// Middlewares and static files declaration
+app.use(express.static('public'))
+app.use(express.urlencoded())
+
+// Connect to database
+const dbURI = "mongodb://localhost:27017/BlogDB";
+mongoose.connect(dbURI).then((result) => {
+    console.log("Connected to Mongoose Database");
+
+    // Listen for requests
+    app.listen(3000);
+}).catch((e) => {
+    console.log(e)
+})
+
+// Mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) =>{
+    const blog = new Blog({
+        title: "New Blog 2",
+        snippet: "About new Blog",
+        body: "Blog Body"
+    });
+
+    blog.save().then((result) => {
+        res.send(result)
+    })
+} )
+
+app.get("/get-blog", (req, res) => {
+    Blog.find().then((result) => {
+        res.send(result)
+    }).catch((e) => {
+        console.log(e)
+    })
+})
+
+app.get("/single-blog", (req, res) => {
+    Blog.findById("68b1c3d98d8e2126639ca365").then((result) => {
+        res.send(result);
+    }).catch((e) => {
+        console.log(e)
+    })
+})
+
+app.get("/blogs", (req, res) => {
+    Blog.find().sort({createdAt: -1}).then((result) => {
+        res.render("index", {title: 'Home', blogs: result})
+    }).catch((e)  => {
+        console.log(e)
+    })
+})
+
+app.post("/blogs", (req, res) => {
+    const blog = new Blog(req.body)
+    blog.save().then((result) => {
+        res.redirect("/blogs")
+    }).catch((e) => {
+        console.log(e);
+    })
+})
+
+app.get("/blogs/:id", (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id).then((result => {
+        res.render("details", {blog: result, title: "Blog Details"})
+    })).catch((e) => {
+        console.log(e)
+    })
+});
+
+app.delete("/blogs/:id", (req, res) =>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id).then((result) => {
+        res.json({redirect : '/blogs'})
+    }).catch((e) =>{
+        console.log(e)
+    })
+})
 
 // Log into the console using custom middleware
 // app.use((req, res, next) => {
@@ -17,15 +99,6 @@ app.set('views', __dirname + '/views'); //if views are in a seperate folder
 //     next();
 // })
 
-// Listen for requests
-app.listen(3000)
-
-// Middlewares and static files declaration
-app.use(express.static('public'))
-
-const blogs = [
-  ];
-
 app.use(morgan('tiny'));
 
 app.get('/', (req, res)=>{
@@ -33,7 +106,11 @@ app.get('/', (req, res)=>{
     // res.sendFile('./views/index.html', {root: __dirname})
 
     // instead render a view now
-    res.render("index", {title: 'Home', blogs})
+    // res.render("index", {title: 'Home', blogs})
+
+
+    // REDIRECT IT TO /blogs
+    res.redirect("/blogs")
 })
 
 app.get('/about', (req, res)=>{
